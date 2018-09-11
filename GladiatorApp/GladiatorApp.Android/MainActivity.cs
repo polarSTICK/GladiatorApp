23a09c3dea7bdd2c364103e786c7ff0f43a87a67
Mikael Mäkelä <makelamikael1@gmail.com>
2018-09-11 22:20:01 +0300

    Project brought back to the starting point

--- GladiatorApp/GladiatorApp.Android/MainActivity.cs
+++ GladiatorApp/GladiatorApp.Android/MainActivity.cs
@@ -1,36 +1,18 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
-using Microsoft.AppCenter;
-using Microsoft.AppCenter.Analytics;
-using Microsoft.AppCenter.Crashes;
 
 namespace GladiatorApp.Droid
 {
     [Activity(Label = "GladiatorApp", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : Activity
     {
-        //int count = 1;
-
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            AppCenter.Start("1a3ef1a8-52b8-4221-8659-4295fe591243", typeof(Analytics), typeof(Crashes));
-
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
-
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.myButton);
-
-            button.Click += Button_Click;
-        }
-
-        private void Button_Click(object sender, System.EventArgs e)
-        {
-            Character.Character Mark = new Character.Character(5, 5, 5, 5, 5, 5);
         }
     }
 }
--- GladiatorApp/GladiatorApp.Android/Resources/Resource.designer.cs
+++ GladiatorApp/GladiatorApp.Android/Resources/Resource.designer.cs
@@ -26,27 +26,6 @@ namespace GladiatorApp.Droid
 		
 		public static void UpdateIdValues()
 		{
-			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_dialog_actioned_on_disabled_toast = global::GladiatorApp.Droid.Resource.String.appcenter_distribute_dialog_actioned_on_disabled_toast;
-			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_download_progress_number_format = global::GladiatorApp.Droid.Resource.String.appcenter_distribute_download_progress_number_format;
-			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_downloading_mandatory_update = global::GladiatorApp.Droid.Resource.String.appcenter_distribute_downloading_mandatory_update;
-			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_install = global::GladiatorApp.Droid.Resource.String.appcenter_distribute_install;
-			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_install_ready_message = global::GladiatorApp.Droid.Resource.String.appcenter_distribute_install_ready_message;
-			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_install_ready_title = global::GladiatorApp.Droid.Resource.String.appcenter_distribute_install_ready_title;
-			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_notification_category = global::GladiatorApp.Droid.Resource.String.appcenter_distribute_notification_category;
-			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_unknown_sources_dialog_message = global::GladiatorApp.Droid.Resource.String.appcenter_distribute_unknown_sources_dialog_message;
-			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_unknown_sources_dialog_settings = global::GladiatorApp.Droid.Resource.String.appcenter_distribute_unknown_sources_dialog_settings;
-			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_update_dialog_download = global::GladiatorApp.Droid.Resource.String.appcenter_distribute_update_dialog_download;
-			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_update_dialog_message_mandatory = global::GladiatorApp.Droid.Resource.String.appcenter_distribute_update_dialog_message_mandatory;
-			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_update_dialog_message_optional = global::GladiatorApp.Droid.Resource.String.appcenter_distribute_update_dialog_message_optional;
-			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_update_dialog_postpone = global::GladiatorApp.Droid.Resource.String.appcenter_distribute_update_dialog_postpone;
-			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_update_dialog_title = global::GladiatorApp.Droid.Resource.String.appcenter_distribute_update_dialog_title;
-			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_update_dialog_view_release_notes = global::GladiatorApp.Droid.Resource.String.appcenter_distribute_update_dialog_view_release_notes;
-			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_update_failed_dialog_ignore = global::GladiatorApp.Droid.Resource.String.appcenter_distribute_update_failed_dialog_ignore;
-			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_update_failed_dialog_message = global::GladiatorApp.Droid.Resource.String.appcenter_distribute_update_failed_dialog_message;
-			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_update_failed_dialog_reinstall = global::GladiatorApp.Droid.Resource.String.appcenter_distribute_update_failed_dialog_reinstall;
-			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_update_failed_dialog_title = global::GladiatorApp.Droid.Resource.String.appcenter_distribute_update_failed_dialog_title;
-			global::Microsoft.AppCenter.Push.Android.Resource.Drawable.ic_stat_notify_dot = global::GladiatorApp.Droid.Resource.Drawable.ic_stat_notify_dot;
-			global::Microsoft.AppCenter.Push.Android.Resource.String.library_name = global::GladiatorApp.Droid.Resource.String.library_name;
 		}
 		
 		public partial class Attribute
@@ -62,43 +41,11 @@ namespace GladiatorApp.Droid
 			}
 		}
 		
-		public partial class Drawable
-		{
-			
-			// aapt resource value: 0x7f020000
-			public const int ic_stat_notify_dot = 2130837504;
-			
-			static Drawable()
-			{
-				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
-			}
-			
-			private Drawable()
-			{
-			}
-		}
-		
-		public partial class Id
-		{
-			
-			// aapt resource value: 0x7f060000
-			public const int myButton = 2131099648;
-			
-			static Id()
-			{
-				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
-			}
-			
-			private Id()
-			{
-			}
-		}
-		
 		public partial class Layout
 		{
 			
-			// aapt resource value: 0x7f040000
-			public const int Main = 2130968576;
+			// aapt resource value: 0x7f030000
+			public const int Main = 2130903040;
 			
 			static Layout()
 			{
@@ -113,8 +60,8 @@ namespace GladiatorApp.Droid
 		public partial class Mipmap
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int Icon = 2130903040;
+			// aapt resource value: 0x7f020000
+			public const int Icon = 2130837504;
 			
 			static Mipmap()
 			{
@@ -129,71 +76,11 @@ namespace GladiatorApp.Droid
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f050015
-			public const int app_name = 2131034133;
+			// aapt resource value: 0x7f040001
+			public const int app_name = 2130968577;
 			
-			// aapt resource value: 0x7f050001
-			public const int appcenter_distribute_dialog_actioned_on_disabled_toast = 2131034113;
-			
-			// aapt resource value: 0x7f050002
-			public const int appcenter_distribute_download_progress_number_format = 2131034114;
-			
-			// aapt resource value: 0x7f050003
-			public const int appcenter_distribute_downloading_mandatory_update = 2131034115;
-			
-			// aapt resource value: 0x7f050004
-			public const int appcenter_distribute_install = 2131034116;
-			
-			// aapt resource value: 0x7f050005
-			public const int appcenter_distribute_install_ready_message = 2131034117;
-			
-			// aapt resource value: 0x7f050006
-			public const int appcenter_distribute_install_ready_title = 2131034118;
-			
-			// aapt resource value: 0x7f050007
-			public const int appcenter_distribute_notification_category = 2131034119;
-			
-			// aapt resource value: 0x7f050008
-			public const int appcenter_distribute_unknown_sources_dialog_message = 2131034120;
-			
-			// aapt resource value: 0x7f050009
-			public const int appcenter_distribute_unknown_sources_dialog_settings = 2131034121;
-			
-			// aapt resource value: 0x7f05000a
-			public const int appcenter_distribute_update_dialog_download = 2131034122;
-			
-			// aapt resource value: 0x7f05000b
-			public const int appcenter_distribute_update_dialog_message_mandatory = 2131034123;
-			
-			// aapt resource value: 0x7f05000c
-			public const int appcenter_distribute_update_dialog_message_optional = 2131034124;
-			
-			// aapt resource value: 0x7f05000d
-			public const int appcenter_distribute_update_dialog_postpone = 2131034125;
-			
-			// aapt resource value: 0x7f05000e
-			public const int appcenter_distribute_update_dialog_title = 2131034126;
-			
-			// aapt resource value: 0x7f05000f
-			public const int appcenter_distribute_update_dialog_view_release_notes = 2131034127;
-			
-			// aapt resource value: 0x7f050010
-			public const int appcenter_distribute_update_failed_dialog_ignore = 2131034128;
-			
-			// aapt resource value: 0x7f050011
-			public const int appcenter_distribute_update_failed_dialog_message = 2131034129;
-			
-			// aapt resource value: 0x7f050012
-			public const int appcenter_distribute_update_failed_dialog_reinstall = 2131034130;
-			
-			// aapt resource value: 0x7f050013
-			public const int appcenter_distribute_update_failed_dialog_title = 2131034131;
-			
-			// aapt resource value: 0x7f050014
-			public const int hello = 2131034132;
-			
-			// aapt resource value: 0x7f050000
-			public const int library_name = 2131034112;
+			// aapt resource value: 0x7f040000
+			public const int hello = 2130968576;
 			
 			static String()
 			{
